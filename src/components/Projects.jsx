d5c0a741bc8fe4a1da280e2f546c394da857ffe3
Arshiya Singh <singharshiya28@gmail.com>
2026-06-24 02:07:24 +0530

    debugging

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import Tilt from 'react-parallax-tilt';
 import { useTheme } from './ThemeContext';
 
+const thumb-mediqueue.png = '/thumb-mediqueue.png';
 const PrimaryProjects = [
 
     {
